b7653695efe2f5bf8252c10c13dfd126a6b23910
karlwebstersponge <19327709+ktasper@users.noreply.github.com>
2022-06-01 16:18:51 +0100

    finished broken image tests

--- tests/test_broken_images.py
+++ tests/test_broken_images.py
@@ -5,6 +5,7 @@ from config.test_settings import Settings as site_settings
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 import time
+import requests
 
 # Test we can open the page and the header says "Broken Images"
 def test_elements_page_heading(browser, broken_images_page):
@@ -24,5 +25,8 @@ def test_images_work(browser, broken_images_page):
         IE: https://the-internet.herokuapp.com/asdf.jpg
         """
         image_urls.append(image.get_property("src"))
-    # Check the response codes to see if we can load the images
-    print (image_urls)
+    # Check the response codes to see if we can load the images 404 = bad
+    for image_url in image_urls:
+        x = requests.get(image_url)
+        if x.status_code == 404:
+            assert False, "%s returned with %s" % (image_url, str(x.status_code))
